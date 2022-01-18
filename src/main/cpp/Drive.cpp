4009c41247c65a03f6f072ab604fb98a6688c062
fayestover <54868990+fayestover@users.noreply.github.com>
2022-01-17 20:15:46 -0500

    Drive

--- src/main/cpp/Drive.cpp
+++ src/main/cpp/Drive.cpp
@@ -1,7 +1,34 @@
 #include "Drive.h"
+#include "rev/CANSparkMax.h"
 
 using namespace std;
 
 Drive::Drive(){
+    int leftdriveID = 9;
+    int leftdriveID2 = 10;
+    int rightdriveID = 7;
+    int rightdriveID2 = 8;
+
+    // Define motors, sensors, and pneumatics here
+    m_leftdrive = new rev::CANSparkMax{leftdriveID, rev::CANSparkMax::MotorType::kBrushless}; //actually 1
+    m_leftdrive2 = new rev::CANSparkMax{leftdriveID2, rev::CANSparkMax::MotorType::kBrushless};
+    m_leftdrive->SetInverted(true);
+    m_leftdrive2->SetInverted(true);
+    m_rightdrive = new rev::CANSparkMax{rightdriveID, rev::CANSparkMax::MotorType::kBrushless};
+    m_rightdrive2 = new rev::CANSparkMax{rightdriveID2, rev::CANSparkMax::MotorType::kBrushless};
 }
 
+/* JOYSTICK DRIVE */
+/* This function provides basic joystick control of the drive base*/
+    
+    void Drive::Joystick_Drive(double LeftStick, double RightStick){
+
+    double left_out = LeftStick*LeftStick*LeftStick;
+    double right_out = RightStick*RightStick*RightStick;
+
+    m_leftdrive -> Set(left_out);
+    m_leftdrive2 -> Set(left_out);
+    m_rightdrive -> Set(right_out);
+    m_rightdrive2 -> Set(right_out);
+
+}
--- src/main/include/Drive.h
+++ src/main/include/Drive.h
@@ -1,5 +1,7 @@
 #ifndef Drive_H
 #define Drive_H
+#include "Drive.h"
+#include "rev/CANSparkMax.h"
 
 #pragma once
 
@@ -8,8 +10,16 @@ using namespace std;
 
 class Drive {
     private:
+        // Define motor, sensors, and pnematic pointers here
+
+        rev::CANSparkMax * m_leftdrive;
+        rev::CANSparkMax * m_leftdrive2;
+        rev::CANSparkMax * m_rightdrive;
+        rev::CANSparkMax * m_rightdrive2;
 
     public:
         Drive();
+        void Joystick_Drive(double LeftStick, double RightStick);
 };
+
 #endif
