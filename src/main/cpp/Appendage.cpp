99368e5a0e179a4a123eebc21d39c139a24a9317
Pig <23kongw@brrice.edu>
2022-01-18 19:10:12 -0500

    Fill in Appendage

--- src/main/cpp/Appendage.cpp
+++ src/main/cpp/Appendage.cpp
@@ -2,6 +2,62 @@
 
 using namespace std;
 
-Appendage::Appendage(){
+Appendage::Appendage()
+{
+    int m_IntakeId = 3;
+    int p_IntakeId_a = 14;
+    int p_IntakeId_b = 15;
+    int m_ShooterId = 6;
+    // Define motors, sensors, and pneumatics here
+    m_Intake = new rev::CANSparkMax{m_IntakeId, rev::CANSparkMax::MotorType::kBrushless}; // actually 1
+    p_Intake = new frc::DoubleSolenoid{frc::PneumaticsModuleType::REVPH, p_IntakeId_a, p_IntakeId_b};
+    m_Shooter = new rev::CANSparkMax{m_ShooterId, rev::CANSparkMax::MotorType::kBrushless};
 }
 
+/*
+ * Allows robot to Intake Balls
+ */
+void Appendage::Intake_In()
+{
+    m_Intake->Set(1);
+}
+
+/*
+ * Allows robot to Intake Balls (Reverse)
+ */
+void Appendage::Intake_Out()
+{
+    m_Intake->SetInverted(1);
+}
+
+/*
+ * Turns off
+ */
+void Appendage::Intake_Off()
+{
+    m_Intake->Set(0);
+}
+
+/*
+ * Allows robots intake to go up
+ */
+void Appendage::Intake_Up()
+{
+    p_Intake->Set(frc::DoubleSolenoid::Value::kForward);
+}
+
+/*
+ * Allows robots intake to go down
+ */
+void Appendage::Intake_Down()
+{
+    p_Intake->Set(frc::DoubleSolenoid::Value::kReverse);
+}
+
+/*
+ * Allows shooter to position when shooting
+ */
+int Appendage::Shooter_Encoder(){
+    double output;
+    return output;
+}
--- src/main/include/Appendage.h
+++ src/main/include/Appendage.h
@@ -2,14 +2,29 @@
 #define Appendage_H
 
 #pragma once
-
+#include "rev/CANSparkMax.h"
+#include "rev/CANEncoder.h"
+#include <frc/DoubleSolenoid.h>
 
 using namespace std;
 
-class Appendage {
-    private:
+class Appendage
+{
+private:
+    // Define motor, sensors, and pnematic pointers here
+    rev::CANSparkMax *m_Intake;
+    frc::DoubleSolenoid *p_Intake;
+    rev::CANSparkMax *m_Shooter;
+    rev::CANEncoder *s_Shooter_Encoder;
 
-    public:
-        Appendage();
+public:
+    Appendage();
+    void Intake_In();
+    void Intake_Out();
+    void Intake_Off();
+    void Intake_Up();
+    void Intake_Down();
+
+    int Shooter_Encoder();
 };
 #endif
