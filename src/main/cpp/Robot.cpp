875dd0d163e2c7afbbbaf2b5ae59ff0282506dd8
fayestover <54868990+fayestover@users.noreply.github.com>
2022-01-19 19:49:58 -0500

    teleop drive

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -18,8 +18,13 @@ also: merge into main before each event, Before event create event branch and me
 #include "Appendage.h"
 #include "Led.h"
 #include <fmt/core.h>
-
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "wpi/span.h"
 #include <frc/smartdashboard/SmartDashboard.h>
+
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
@@ -69,9 +74,84 @@ void Robot::AutonomousPeriodic() {
   }
 }
 
-void Robot::TeleopInit() {}
+void Robot::TeleopInit() {
 
-void Robot::TeleopPeriodic() {}
+  drive_straight_first = true;
+
+}
+void Robot::TeleopPeriodic() {
+
+  //********** Read in Joystick Values ******************************************
+  //------------- Driver Controller ---------------------------------------------
+
+  double c1_joy_leftdrive = controller1.GetRawAxis(1);
+  double c1_joy_rightdrive = controller1.GetRawAxis(5);
+  //bool c1_btn_back = controller1.GetRawButton(7);
+  //bool c1_btn_start = controller1.GetRawButton(8);
+  //double c1_righttrigger = controller1.GetRawAxis(3);
+  //double c1_lefttrigger = controller1.GetRawAxis(2);
+  //bool c1_leftbmp = controller1.GetRawButton(5);
+  //bool c1_rightbmp = controller1.GetRawButton(6);
+  bool c1_btn_b = controller1.GetRawButton(2);
+  //bool c1_btn_x = controller1.GetRawButton(3);
+  bool c1_btn_a = controller1.GetRawButton(1);
+
+  //-----------------------------------------------------------------------------
+  //------------ Operator Controller --------------------------------------------
+  /*double c2_joy_left = controller2.GetRawAxis(1);
+  bool c2_btn_a = controller2.GetRawButton(1);
+  bool c2_btn_b = controller2.GetRawButton(2);
+  bool c2_btn_y = controller2.GetRawButton(4);
+  bool c2_btn_x = controller2.GetRawButton(3);
+  bool c2_btn_lb = controller2.GetRawButton(5);
+  bool c2_btn_rb = controller2.GetRawButton(6);
+  double c2_dpad = controller2.GetPOV(0);
+  bool c2_btn_back = controller2.GetRawButton(7);
+  bool c2_btn_start = controller2.GetRawButton(8);
+  bool c2_rightbumper = controller2.GetRawButton(6);
+  bool c2_leftbumper = controller2.GetRawButton(5);
+  bool c2_right_trigger = controller2.GetRawAxis(3);
+  bool c2_left_trigger = controller2.GetRawAxis(2);*/
+
+  //----------------------------------------------------------------------------
+ 
+  // Read in camera Stuff
+  
+  std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  table->PutNumber("ledMode", 1);
+  table->PutNumber("camMode", 0);
+
+  // -----------PIPELINE STUFF-----------//
+  table->PutNumber("pipeline", 0);
+
+  //--------CAMERA VALUES-----------------//
+  float camera_x = table->GetNumber("tx", 0);
+    
+  //float camera_exist = table->GetNumber("tv", 0);
+  //float image_size = table->GetNumber("ta", 0);
+  //float camera_y = table->GetNumber("ty", 0);
+  //float camera_s = table->GetNumber("ts", 0);
+
+  /* DRIVE CODE */
+
+  if (c1_btn_b){
+    MyDrive.drive_straight(drive_straight_first, c1_joy_leftdrive);
+
+    drive_straight_first = false;
+  }
+
+    else if (c1_btn_a){
+      MyDrive.camera_intake(camera_x, c1_joy_leftdrive);
+    }
+
+    else{
+      drive_straight_first = true;
+
+      MyDrive.Joystick_Drive(c1_joy_leftdrive, c1_joy_rightdrive);
+
+    }
+
+  }
 
 void Robot::DisabledInit() {}
 
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -5,12 +5,26 @@
 #pragma once
 
 #include <string>
-
+#include "Drive.h"
+#include "Appendage.h"
+#include "Led.h"
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
+#include <frc/Joystick.h>
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "wpi/span.h"
+
 
 class Robot : public frc::TimedRobot {
+
  public:
+
+  //Include subsystem object definitions here
+  Drive MyDrive; 
+
   void RobotInit() override;
   void RobotPeriodic() override;
   void AutonomousInit() override;
@@ -22,9 +36,21 @@ class Robot : public frc::TimedRobot {
   void TestInit() override;
   void TestPeriodic() override;
 
+
+
+
  private:
+
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
+
+  frc::Joystick controller1{0}; // Driver controller
+  frc::Joystick controller2{1}; // Operator controller
+
+
+  bool drive_straight_first;
+
+ 
 };
