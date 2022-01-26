1b2e6cbb6a7fd9eb14da4da906c54bd4c6aa9507
Pig <23kongw@brrice.edu>
2022-01-25 19:14:28 -0500

    Add Paths

---
+++ src/main/include/Paths.h
@@ -0,0 +1,19 @@
+#ifndef Paths_H
+#define Paths_H
+
+#pragma once
+
+using namespace std;
+
+class Paths {
+
+ private:
+
+
+ public:
+  Paths();
+  double ReturnTableVal(int count, int select, int brian);
+ 
+
+};
+#endif 
