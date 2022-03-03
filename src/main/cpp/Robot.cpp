a1243b29100a9a246d2256b4970e9e0661a09a94
Eric Savage <savage301@gmail.com>
2022-03-03 15:13:07 -0500

    Update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -806,7 +806,6 @@ else{
 
 frc::SmartDashboard::PutBoolean("Endgame State", endgame_unlock);
 frc::SmartDashboard::PutBoolean("Shooter Test State", shooter_test);
-frc::SmartDashboard::PutBoolean("C2 Btn Y", c2_btn_y);
 frc::SmartDashboard::PutBoolean("Shooter At Speed", atspeed);
 frc::SmartDashboard::PutBoolean("Shooter Aligned", align);
 frc::SmartDashboard::PutNumber("Camera Distance", distance);
